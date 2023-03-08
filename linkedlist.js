6c5af06bb61cca2fe32e8a59f1b58f045a5e94a7
IrvingGlt <41974385+Krpeta21@users.noreply.github.com>
2023-03-07 22:48:31 -0600

    🛠  refactor: Otro metodo para aplicar linkedlist.

--- linkedlist.js
+++ linkedlist.js
@@ -1,40 +1,194 @@
 class Node{
-    constructor(value, prev =null ,next = null){
-        this.value = value;
-        this.next = next;
-        this.prev = prev;
+    constructor(value){
+        this.value = value
+        this.next = null
     }
 }
-
-class LinkedList {
+class LinkedList{
     constructor(){
-        this.head = null;
-        this.tail = null;
+        this.head = null
+        this.tail = null
+        this.size = 0
     }
-
+    
+    isEmpty(){
+        return this.size === 0
+    }
+    getSize(){
+        return this.size
+    }
+    
+    prepend(value){
+        const node = new Node(value)
+        if(this.isEmpty()){
+            this.head = node
+            this.tail = node
+        }else{
+            node.next = this.head
+            this.head = node
+        }
+        this.size++
+    }
     append(value){
-         const newNode = new Node(value);
-         if(!this.head){
-            this.head = newNode;
-         }else{
-            this.tail.next = newNode;
-            newNode.prev = this.tail;
-         }
-         this.tail = newNode;
+        const node = new Node(value)
+        if(this.isEmpty()){
+            this.head = node
+            this.tail = node
+        }else{
+            this.tail.next = node
+            this.tail = node
+        }
+        this.size++
     }
-    traverse(callback){
-        let currentNode = this.head;
-        while(currentNode !== null){
-            callback(currentNode);
-            currentNode = currentNode.next;
+    
+    removeFromFront(){
+        if(this.isEmpty()){
+            return null
+        }
+        const value = this.head.value
+        this.head = this.head.next
+        this.size--
+        return value
+    }
+    removeFromEnd(){
+        if(this.isEmpty()){
+            return null
+        }
+        const value = this.tail.value
+        
+        if(this.size === 1){
+            this.head = null
+            this.tail = null
+        }else{
+            let prev = this.head
+            while(prev.next !== this.tail){
+                prev = prev.next
+            }
+            prev.next = null
+            this.tail = prev
+        }
+        this.size--
+        return value
+    }
+    
+    print(){
+        if(this.isEmpty()){
+            console.log('The list is empty')
+        }else {
+            let curr = this.head
+            let listValues = ''
+        while(curr){
+        listValues += `${curr.value} `
+        curr = curr.next
+        }
+        console.log(listValues)
         }
     }
+    insert(value,index){
+     if(index < 0 || index > this.size){
+         return console.log(`No se puede hacer esta accion.`)
+     }
+     if(index === 0){
+         this.prepend(value)
+     }else{
+         const node = new Node(value)
+         let prev = this.head
+         for(let i=0; i< index-1;i++){
+             prev = prev.next
+         }
+         node.next = prev.next
+         prev.next = node
+         this.size++
+     } 
+    }
+    
+    removeFrom(index){
+    if(index < 0 || index >= this.size){
+    return null
+    }
+    let removedNode
+    if(index == 0){
+        removedNode = this.head
+        this.head = this.head.next
+    }else{
+        let prev = this.head
+        for(let i=0; i< index -1;i++){
+            prev = prev.next
+        }
+        removedNode = prev.next
+        prev.next = removedNode.next
+        }
+    this.size--
+    return removedNode.value
+    }
+    removeValue(value){
+        if(this.isEmpty()){
+            return null
+        }
+        if(this.head.value === value){
+            this.head = this.head.next
+            this.size--
+            return value
+        }else{
+            let prev = this.head
+            while(prev.next && prev.next.value !== value){
+                prev = prev.next
+            }
+            if(prev.next){
+            const removedNode = prev.next
+            prev.next = removedNode.next
+                this.size--
+                return value
+            }
+            return null
+        }
+    }
+    search(value){
+        if(this.isEmpty()){
+            return -1
+        }
+        if(this.head.value === value){
+            return 0
+        }else{
+            let i = 0
+            let curr = this.head
+            while(curr){
+                if(curr.value === value){
+                    return i
+                }
+                curr = curr.next
+                i++
+            }
+            return -1
+        }
+    }
+    reverse(){
+        let prev = null
+        let curr = this.head
+        let cola = this.head
+        while(curr){
+         let next = curr.next
+         curr.next = prev
+         prev = curr
+         curr = next
+        }
+        this.head = prev
+        this.tail = cola
+    }
+    
 }
-const printNode = (node) => console.log(`Valor: ${node.value} | Next ${node.next.value}`)
 
 const list = new LinkedList()
+
 list.append(1)
 list.append(2)
 list.append(3)
+list.append(4)
+list.prepend(0)
 
-list.traverse(printNode)
+list.reverse()
+list.print()
+list.removeFromEnd()
+
+
+list.print()
